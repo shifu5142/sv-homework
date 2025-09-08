a91786533cc5dd64125ca61af099fc058c1bd42d
SHIFU <tomer5142200@walla.co.il>
2025-09-08 11:38:10 +0300

    homework-11

--- script.js
+++ script.js
@@ -1,64 +1,85 @@
 "use strict";
 //1
-const checkId = function () {
-  let idInput = document.getElementById("centered").value;
-  if (idInput.length !== 9) {
-    console.log(false);
-    return;
-  }
-  for (let i = 0; i < idInput.length; i++) {
-    if (idInput.charAt(i) >= "0" && idInput.charAt(i) <= "9") {
-    } else {
-      console.log(false);
-      break;
-    }
-  }
+const arrayLength = (arr) => {
+  if (arr.length >= 5) return true;
+  return false;
 };
+console.log(arrayLength([3, 5, 3, 2, 5, 3]));
 //2
-const checjNum = function (num) {
-  if (num % 1 == 0 && num % num == 0) {
-    console.log(`המספר ${num} הוא ראשוני`);
-  }
+const checkFirstLastChar = (num) => {
+  if (num.charAt(0) === num.charAt(num.length - 1))
+    return "the first letter and the last are the same";
+  return "the first letter and the last are not the same";
 };
-checjNum(5);
+console.log(checkFirstLastChar("Hello"));
 //3
-const checStr = function (str) {
-  let b = "";
-  for (let i = 0; i < str.length; i++) {
-    let char = str.charAt(i);
-    if (str.charAt(i) === str.charAt(i).toUpperCase()) {
-      b += " " + char;
-    } else {
-      b += char;
-    }
-  }
-  return console.log(b);
+const lastIsUpperCase = (str) => {
+  if (str.charAt(str.length - 1) === str.charAt(str.length - 1).toUpperCase())
+    return true;
+  return false;
 };
-checStr("helLLLLLLLo");
-
-//4
-function insideStr(subStr, mainStr) {
-  let count = 0;
-  let position = 0;
-
-  while (position < mainStr.length) {
-    if (mainStr.includes(subStr, position)) {
-      count++;
-      position = mainStr.indexOf(subStr, position) + subStr.length;
+console.log(lastIsUpperCase("HELLo"));
+/////////////////////////////////////////////////////////////////////////////////
+//part 2
+//////////////////////////////////////////////////////////////////////////////
+//1
+const calcDevide = function (arr) {
+  let ifTrue = false;
+  arr.forEach((element, i) => {
+    if (element % 3 === 0) {
+      console.log(`the element at postion${i} is devide at 3`);
+      ifTrue = true;
     } else {
-      break;
+      console.log(`the element at postion${i} isn't devide at 3`);
     }
-  }
-
-  return console.log(count);
-}
-
-insideStr("h", "hellohhhhhhhhhhhh");
-//6
-const displayInput = document.querySelector(".display");
-const buttonAll = document.querySelectorAll("button");
-const calcBut = function () {
-  buttonAll.forEach((element) => {
-    element.target.addEventListener("click", function () {});
   });
+  if (ifTrue) return;
+  return "The array is not divisible by 3";
 };
+console.log(calcDevide([3, 4, 4, 4, 3, 2, 4, 5]));
+//2
+let letArr = ["A", "B", "c", "d", "E"];
+const newArr = letArr.map((value) => {
+  if (value === value.toUpperCase()) return "U";
+  else {
+    return "L";
+  }
+});
+console.log(newArr);
+//3
+const replacePosition = function (arr) {
+  let arrFix = arr.map((value, i) => {
+    if (i % 2 === 0) return (value = i);
+    else {
+      return value;
+    }
+  });
+  return arrFix;
+};
+console.log(replacePosition(["a", "b", "c", "d", "e", "f"]));
+//4
+const calcAge = function (arr) {
+  let arrFix = arr.filter((num) => num > 18);
+  return arrFix;
+};
+
+console.log(calcAge([43, 23, 12, 124, 23, 4, 3, 6]));
+//5
+const filterPos = function (arr) {
+  let arrFix = arr.filter((num, i) => i !== 3);
+  return arrFix;
+};
+
+console.log(filterPos([2, 4, 1, 2, 7, 2, 8]));
+//6
+let arr = ["hello", "arr", "bor", "bon"];
+const addStr = function (str) {
+  arr = [...arr, str];
+  return arr;
+};
+console.log(addStr("hello"));
+//7
+const spreadArr = function (arr1, arr2) {
+  return (arr1 = [...arr1, ...arr2]);
+};
+console.log(spreadArr(["hello", "you"], ["are", "my", "friend"]));

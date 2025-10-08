42b2682a956946f237bcdacf1c3fcb417ddc1afc
SHIFU <tomer5142200@walla.co.il>
2025-10-08 10:14:43 +0300

    homework-13

--- script.js
+++ script.js
@@ -1,215 +1,161 @@
 "use strict";
+//class part
+class Drive {
+  constructor(renterName, date, rentKm) {
+    this.renterName = renterName;
+    this.date = date;
+    this.rentKm = rentKm;
+  }
+}
+
+class Car {
+  constructor(compName, modelName, carMil) {
+    this.compName = compName;
+    this.modelName = modelName;
+    this.availableCar = true;
+    this.carMil = carMil;
+  }
+  static drives = [
+    new Drive("Alice", "2025-09-30", 120),
+    new Drive("Bob", "2025-09-25", 300),
+    new Drive("Charlie", "2025-09-20", 180),
+  ];
+
+  kilNum = "";
+  calcKm() {
+    return (this.rentKm = Car.drives.reduce((cur, acc) => (acc += cur)));
+  }
+  updateKm(travel) {
+    Car.drives.push(travel);
+    this.calcKm();
+  }
+}
+class CompanyCars {
+  constructor(compName) {
+    this.compName = compName;
+  }
+  static companyCarArr = [
+    new Car("Toyota", "Corolla", 120000),
+    new Car("Honda", "Civic", 80000),
+    new Car("Ford", "Focus", 60000),
+  ];
+  addCar(car) {
+    CompanyCars.companyCarArr.push(car);
+  }
+  highestKil() {
+    let bigestNum;
+    CompanyCars.companyCarArr.forEach((acc) => {
+      if (bigestNum.kilNum <= acc.kilNum) bigestNum = acc;
+    });
+    console.log(
+      `The car with the highest kilometer is ${bigestNum} and the kilometer is ${bigestNum.kilNum} `
+    );
+  }
+  availableCar() {
+    const availableCarArr = CompanyCars.companyCarArr.map(
+      (value) => value.availableCar
+    );
+    console.log(`All the available cars are ${availableCarArr.join(", ")}`);
+  }
+  addDrive(compName, driveObj) {}
+}
+
+//part 2
 //1
-const showFamily = function (firstName, lastName) {
-  let firstP = document.createElement("p");
-  firstP.textContent = firstName;
-  firstP.style.color = "red";
-  firstP.style.textDecoration = "underline";
-  firstP.style.textAlign = "center";
-  let secondP = document.createElement("p");
-  secondP.textContent = lastName;
-  secondP.style.fontSize = "50px";
-  secondP.style.border = "solid 1px black";
-  secondP.style.borderRadius = "10px";
-  secondP.style.padding = "10px";
-  let formName = document.createElement("div");
-  formName.style.background = "#fff";
-  formName.style.padding = "32px 100px";
-  formName.style.borderRadius = "50px";
-  formName.style.boxShadow = "5px 5px 15px rgba(0, 0, 0, 0.3)";
-  formName.style.width = "100";
-  formName.className = "formName";
-  formName.appendChild(firstP);
-  formName.appendChild(secondP);
-  document.body.appendChild(formName);
+let missionImp = document.querySelector(".text-input");
+const missionLis = document.querySelector(".mission-list");
+let missionArr = [];
+let i = 1;
+const addMission = function () {
+  if (!missionImp.value) alert("the input is empty");
+  missionLis.innerHTML += `<li>Mission ${i}: ${missionImp.value}</li>`;
+  i++;
+  missionArr.push(missionImp.value);
+  localStorage.setItem("todos", missionArr);
+  missionImp.value = "";
 };
-showFamily("tomer", "farag");
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("todos")) {
+    const todos = localStorage.getItem("todos").split(",");
+    console.log(todos);
+    missionLis.innerHTML += `the previos mission left:`;
+    todos.forEach(
+      (value, i) =>
+        (missionLis.innerHTML += `<li>Missions number:${i + 1} ${value}</li>`)
+    );
+  }
+});
+const deleteMem = function () {
+  localStorage.removeItem("todos");
+  missionLis.innerHTML = "";
+};
 //2
-const products = [
-  { name: "Apple", description: "Fresh and tasty", price: 12.34 },
-  { name: "Banana", description: "Organic product", price: 8.99 },
-  { name: "Carrot", description: "Best quality", price: 5.5 },
-  { name: "Doughnut", description: "Limited edition", price: 15.2 },
-  { name: "Egg", description: "Special offer", price: 3.75 },
-];
-let amount = document.querySelector(".amount");
-const section2 = document.querySelector(".part2");
-let firstP = document.createElement("div");
-const filtrerProd = function (e) {
+let inputName = document.querySelector(".inputs-wrapper").children[1];
+let inputEmail = document.querySelector(".inputs-wrapper").children[2];
+const infSave = function (e) {
   e.preventDefault();
-  firstP.innerHTML = "";
-  let inputAmount = document.querySelector(".amount").value;
-  if (isNaN(inputAmount)) {
-    alert("Please enter a valid number");
-    amount.value = "";
-    return;
-  }
-  const num = Number(inputAmount);
-  let newArr = products.filter(
-    (value) => value.price === num || value.price < num
-  );
-  amount.value = "";
-
-  console.log(newArr);
-
-  firstP.innerHTML = newArr
-    .map(
-      (product) =>
-        `<p><strong>${product.name}</strong>: ${product.description} - $${product.price}</p>`
-    )
-    .join("");
-  section2.appendChild(firstP);
+  if (!inputName.value || !inputEmail.value)
+    alert("The inputes or on of them are empty");
+  let formDraft = {
+    name: inputName.value,
+    email: inputEmail.value,
+  };
+  sessionStorage.setItem("formDraft", JSON.stringify(formDraft));
 };
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-//1
-const books = [
-  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-  { title: "1984", author: "George Orwell", year: 1949 },
-  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
-  { title: "Moby Dick", author: "Herman Melville", year: 1851 },
-];
-const section3 = document.querySelector(".part3");
-let bookWindow = document.createElement("div");
-const checjBook = function (authorName) {
-  authorName.preventDefault();
-  bookWindow.innerHTML = "";
-  section3.appendChild(bookWindow);
-  let getAuthor = document.querySelector(".author-name");
-  let newBooks = books.filter((authorN) => authorN.author == getAuthor.value);
-
-  if (newBooks.length === 0) {
-    alert("The book author doesn't exist");
-    getAuthor.value = "";
+document.addEventListener("DOMContentLoaded", () => {
+  if (sessionStorage.getItem("formDraft")) {
+    inputName.value = JSON.parse(sessionStorage.getItem("formDraft")).name;
+    inputEmail.value = JSON.parse(sessionStorage.getItem("formDraft")).email;
   }
+});
 
-  bookWindow.innerHTML = newBooks
-    .map(
-      (value) =>
-        `<p><strong>${value.title}</strong></p> <p>${value.author}</p>  <p>${value.year}</p>`
-    )
-    .join("");
-  let secondp = bookWindow.querySelector(":nth-child(2)");
-  bookWindow.style.display = "flex";
-  bookWindow.style.flexDirection = "column";
-  bookWindow.style.backgroundColor = "#f0f0f0";
-  bookWindow.style.borderRadius = "10px";
-  bookWindow.style.flexWrap = "wrap";
-  secondp.style.color = "blue";
-  bookWindow.lastElementChild.style.textDecoration = "underline ";
-  bookWindow.lastElementChild.style.textDecorationColor = "#7a7878ff";
-  section3.appendChild(bookWindow);
-};
-//2
-const tests = [
-  ["Test 1", [85, 72, 90, 88, 95]],
-  ["Test 2", [60, 100, 78, 82, 91]],
-  ["Test 3", [88, 95, 70, 65, 80]],
-  ["Test 4", [55, 65, 75, 85, 92]],
-  ["Test 5", [99, 98, 97, 95, 94]],
-  ["Test 6", [40, 50, 60, 70, 80]],
-  ["Test 7", [73, 76, 79, 82, 85]],
-  ["Test 8", [20, 35, 45, 55, 65]],
-];
-const gradesAve = document.createElement("div");
-gradesAve.className = "average-row";
-const mathAverage = function (arr = tests) {
-  const arrAverage = arr.map(
-    (value) => value[1].reduce((acc, cur) => (acc += cur)) / value[1].length
-  );
-  console.log(arrAverage);
-  gradesAve.innerHTML = `<div>Grades Average</div>`;
-  arrAverage.forEach(
-    (element) => (gradesAve.innerHTML += `<div>${element}</div>`)
-  );
-  document.querySelector(".table-container").appendChild(gradesAve);
+const deleteInp = function (e) {
+  e.preventDefault();
+  inputName.value = "";
+  inputEmail.value = "";
+  sessionStorage.removeItem("formDraft");
 };
 //3
-const missions = [
-  { id: 1, text: "Collect 10 coins" },
-  { id: 2, text: "Defeat the dragon" },
-  { id: 3, text: "Rescue the villager" },
-  { id: 4, text: "Find the hidden treasure" },
-  { id: 5, text: "Complete the puzzle" },
-  { id: 6, text: "Explore the secret cave" },
-  { id: 7, text: "Gather 5 herbs" },
-  { id: 8, text: "Deliver the message" },
-  { id: 9, text: "Build a shelter" },
-  { id: 10, text: "Win the race" },
-];
-const deleteLink = document.querySelectorAll(".delete-link");
-deleteLink.forEach((button) =>
-  button.addEventListener("click", function (e) {
-    e.preventDefault();
-    const deleteClick = e.target;
-    const deletePar = deleteClick.parentElement;
-    missions.splice(deletePar.id - 1, 1);
-    deletePar.remove();
-    if (missions.length == 0) alert("All missions have complete");
-  })
-);
-//4
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const parseNum = document.querySelector(".arr-Button");
-const numbersDiv = document.querySelector(".numbers");
-const allNumbers = numbersDiv.querySelectorAll("span");
-const replaceText = function () {
-  let i = 0;
-  allNumbers.forEach((value) => {
-    value.innerHTML = numbers[i];
-    i++;
-  });
-};
-const firstFunction = function (arr = numbers) {
-  numbers.sort((a, b) => a - b);
-};
-const secondFunction = function (arr = numbers) {
-  numbers.sort((a, b) => b - a);
-};
-let toggle = true;
-parseNum.addEventListener("click", (e) => {
+let inputUs = document.querySelector(".second-wrap").children[1];
+let inputPass = document.querySelector(".second-wrap").children[2];
+const deletBtn = document.querySelector(".second-btn");
+const divAdd = document.querySelector(".user-inf");
+document.querySelector(".second-log").addEventListener("click", (e) => {
   e.preventDefault();
-  if (toggle) {
-    firstFunction();
-    replaceText();
-  } else {
-    secondFunction();
-    replaceText();
+
+  if (!inputUs.value || !inputPass.value)
+    alert("The inputes or on of them are empty");
+  else {
+    let userData = {
+      username: inputUs.value,
+      password: inputPass.value,
+    };
+    localStorage.setItem("loggedUser", JSON.stringify(userData));
+
+    inputUs.value = "";
+    inputPass.value = "";
+    divAdd.style.opacity = "1";
+    divAdd.innerHTML = `<p>  Hello ${
+      JSON.parse(localStorage.getItem("loggedUser")).username
+    }  you'r connected</p>`;
+    deletBtn.style.display = "block";
   }
-  toggle = !toggle;
 });
-//5
-const colors = [
-  "red",
-  "blue",
-  "green",
-  "yellow",
-  "orange",
-  "purple",
-  "pink",
-  "brown",
-  "black",
-  "white",
-];
-const section7 = document.querySelector(".colors-container");
-const allColors = section7.querySelectorAll("div");
-const creatCol = function (e) {
+deletBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  colors.forEach((value) => {
-    let colorEl = document.createElement("div");
-    colorEl.style.padding = "20px";
-    colorEl.style.borderRadius = "10px";
-    colorEl.style.backgroundColor = value;
-    section7.style.marginTop = "20px";
-    section7.style.display = "flex";
-    section7.appendChild(colorEl);
-  });
-};
-allColors.forEach((value) => {
-  value.addEventListener("click", (e) => {
-    e.preventDefault();
-    const clickDiv = e.target;
-    const allColorsPar = clickDiv.parentElement;
-    allColorsPar.innerHTML += `<p>$</p>`;
-  });
+  inputUs.value = "";
+  inputPass.value = "";
+  localStorage.removeItem("loggedUser");
+  deletBtn.style.display = "none";
+  divAdd.style.opacity = "0";
+  divAdd.innerHTML = "";
 });
-const colorsDiv = document.querySelector();
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("loggedUser")) {
+    divAdd.style.opacity = "1";
+    divAdd.innerHTML = `<p>  Hello ${
+      JSON.parse(localStorage.getItem("loggedUser")).username
+    }  you'r connected</p>`;
+    deletBtn.style.display = "block";
+  }
+});

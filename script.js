5595436e2e60a3f151040c24953938e48f08d137
SHIFU <tomer5142200@walla.co.il>
2025-11-04 10:50:26 +0200

    homework-16

--- script.js
+++ script.js
@@ -1,94 +1,76 @@
-"use strict";
 //1
-const button1=document.querySelector('.link1');
-const button2=document.querySelector('.link2');
-const bigDiv=document.querySelector('.big-div')
-let timer;
- button1.addEventListener('click',(e)=> {
-  e.preventDefault()
-   const promise = new Promise((resolve, reject) => {
-    timer = setTimeout(() => { 
-      bigDiv.style.backgroundColor = '#007bff';
-      bigDiv.style.transition='all 0.3s'
-      bigDiv.querySelector('p').style.color='#fff'
-      resolve('Background color has changed');
-    }, 2000);
-
-    
-    button2.addEventListener('click', () => {
-      clearTimeout(timer);
-      reject('Background color change has been canceled');
-    }, { once: true }); 
-  });
-
-  promise
-    .then((data) => console.log(data))
-    .catch((err) => console.error(err)) 
-})
+const dayjs = require("dayjs");
+const DayParse = function (date) {
+  let dar = dayjs(date);
+  let formatted = dar.format("DD/MM/YYYY");
+  console.log(formatted);
+  console.log(dar.add(30, "day").format("DD/MM/YYYY"));
+  const nowDate = dayjs().format("DD/MM/YYYY");
+  console.log(nowDate);
+  dar.month() === dar.add(30, "day").month()
+    ? console.log("the update date on the same month")
+    : console.log("the update date are not on the same month");
+};
+DayParse("2000-03-13");
 //2
-const timeClock=document.querySelector('.clock')
-const timerCount=function(sec){
-  timeClock.textContent=sec;
-  const data= new Promise((_,reject)=>{   
-   const timer=setInterval(() => {
-    sec--
-      timeClock.textContent=sec
-      if(sec<=0){
-      
-      reject('The timer has end')
-      alert('The timer has end')
-      clearInterval(timer)
-    }
-    }, 1000); 
-});
-  data
-    .then((data) => console.log(data))
-    .catch((err) => console.error(err)) 
-}
-timerCount(10)
-//3
-const customIndexOf =function (string,char) {
-  const promise=new Promise((resolve,reject)=>{
-    for(let i=0;i<=string.length;i++){
-      if(string[i]==char)
-        resolve(`the index of the char is ${[i]}`)
-      
-    }
-    reject(`There isnt any ${char} in the string`)
+const lodashEs = require("lodash-es");
+const grades = [92, 81, 100, 74, 88, 95];
+console.log(lodashEs.max(grades));
+console.log(lodashEs.min(grades));
+console.log(
+  grades.map((value) => {
+    let string = value.toString();
+    string = string[0] + "0";
+    return Number(string);
   })
-  promise.then((data)=>console.log(data)).catch((err)=>console.log(err))
-}
-customIndexOf('hello','o')
-//4
-const tableBody=document.querySelector('tbody')
-tableBody.innerHTML='';
-const getUsers=async function () {
-  try{
-    let data=   await ( fetch('https://jsonplaceholder.typicode.com/users'))
-    data=await data.json();
-    console.log(data);
-  const dataName=data.map((value)=>value.name )
-  let dataAdress=data.map((value)=>value.address.city)
-  console.log(dataName);
-  console.log(dataAdress);
-  data.forEach(item => {
-        const row = document.createElement("tr");
-
-        const nameCell = document.createElement("td");
-        nameCell.textContent = item.name;
-
-        const addressCell = document.createElement("td");
-        addressCell.textContent = item.address.city;
-
-        row.appendChild(nameCell);
-        row.appendChild(addressCell);
-
-        tableBody.appendChild(row);
-      });
-}
-catch(err){
-  console.error(err);
-}
+);
+//3
+const zod = require("zod");
+const stringSchema = zod.string();
+const arr = [
+  { fullName: "Dana Levi", grade: 92 },
+  { fullName: "It", grade: 120 }, // לא תקין
+];
+arr.forEach((value, i) => {
+  if (
+    stringSchema.parse(value.fullName) &&
+    value.fullName.length > 3 &&
+    value.fullName.length <= 25
+  )
+    true;
+  else {
+    return console.log(`The name of the student: ${i + 1} is not valid`);
   }
-  
-getUsers()
+  if (!isNaN(value.grade) && value.grade >= 0 && value.grade <= 100) true;
+  else {
+    return console.log(
+      `The grade of the student: ${value.fullName} is not valid`
+    );
+  }
+});
+//4
+const validator = require("validator");
+const checkValid = function (eArr,uArr) {
+  eArr.forEach((value,i) => {
+   console.log(`The ${i+1} email is: ${validator.isEmail(value)}`); 
+  });
+  uArr.forEach((value,i)=>{
+    console.log(`The ${i+1} url is: ${validator.isURL(value)}` );
+  })
+};
+
+checkValid(["a@b.com", "bad@", "user.name@gmail.com"],["https://example.com", "htp:/nope"]);
+//part 2
+const uuid=require('uuid')
+const addId=function(arr){
+  const usersWithId = arr.map(user => ({
+  ...user,
+  id: uuid.v4(),
+}));
+console.log(usersWithId);
+}
+addId([
+  { fullName: "Dana Levi", grade: 92 },
+  { fullName: "Itay Cohen", grade: 81 },
+])
+
